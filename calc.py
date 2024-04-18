d193b17c92fbfa92310522eb64b55bd213ff65d3
Behzad hamzei <hamzei.behzad>
2024-04-18 10:38:55 -0400

    Version 1.0.0

--- calc.py
+++ calc.py
@@ -1,29 +1,44 @@
-print ("please enter + - * /")
-a=input()
-if a=='+':
-    print ("please enter first number")
-    a=int(input())
-    print ("please enter secend number") 
-    b=int(input())
-    print ("the resulte is = ", a+b)
+print(" _     ___  _   ____  ____  _     ____  _     _     ____  _____  ____  ____ ")
+print("/ \__/|\  \//  /   _\/  _ \/ \   /   _\/ \ /\/ \   /  _ \/__ __\/  _ \/  __\.")
+print("| |\/|| \  /   |  /  | / \|| |   |  /  | | ||| |   | / \|  / \  | / \||  \/|")
+print("| |  || / /    |  \_ | |-||| |_/\|  \_ | \_/|| |_/\| |-||  | |  | \_/||    /")
+print("\_/  \|/_/     \____/\_/ \|\____/\____/\____/\____/\_/ \|  \_/  \____/\_/\_\.")
 
-if a=='-':
-    print ("please enter first number")
-    a=int(input())
-    print ("please enter secend number") 
-    b=int(input())
-    print ("the resulte is = ", a-b)
 
-if a=='*':
-    print ("please enter first number")
-    a=int(input())
-    print ("please enter secend number") 
-    b=int(input())
-    print ("the resulte is = ", a*b)
+while True :
+    print("Please enter the funtction + - * / :")
+    a=input()
+    if a=='+':
+        print ("please enter first number:")
+        a=int(input())
+        print ("please enter secend number:") 
+        b=int(input())
+        print ("the resulte is = ", a+b)
 
-if a=='/':
-    print ("please enter first number")
-    a=int(input())
-    print ("please enter secend number") 
-    b=int(input())
-    print ("the resulte is = ", a/b)
+    if a=='-':
+        print ("please enter first number:")
+        a=int(input())
+        print ("please enter secend number:")
+        b=int(input())
+        print ("the resulte is = ", a-b)
+
+    if a=='*':
+        print ("please enter first number:")
+        a=int(input())
+        print ("please enter secend number:") 
+        b=int(input())
+        print ("the resulte is = ", a*b)
+
+    if a=='/':
+        print ("please enter first number:")
+        a=int(input())
+        print ("please enter secend number:") 
+        b=int(input())
+        if  b==0 :
+            print("the value is not corect")
+            continue
+            print ("the resulte is = ", a/b)
+    print ("Do you want to continue : y/n ")
+    c=input()
+    if c=="n":
+            break
