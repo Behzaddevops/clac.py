cc06af4a4f8ad8b828d83e09b411101300020c69
Behzad hamzei <hamzei.behzad>
2024-04-18 08:37:24 -0400

    add + bug fix

--- calc.py
+++ calc.py
@@ -1,8 +1,8 @@
 print ("please enter + - * /")
 a=input()
-if a=='+'
-print ("please enter first number")
-a=input()
-print ("please enter secend number")
-b=input()
-print ("the resulte is = ", a+b)
+if a=='+':
+    print ("please enter first number")
+    a=input()
+    print ("please enter secend number") 
+    b=input()
+    print ("the resulte is = ", a+b)
