bffd1856d16d976a9ecda588c6aaf61ae74db5c2
Behzad hamzei <hamzei.behzad>
2024-04-18 08:35:10 -0400

    add + function

--- calc.py
+++ calc.py
@@ -1,1 +1,8 @@
-print ("Hello World")
+print ("please enter + - * /")
+a=input()
+if a=='+'
+print ("please enter first number")
+a=input()
+print ("please enter secend number")
+b=input()
+print ("the resulte is = ", a+b)
