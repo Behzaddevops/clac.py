a8654929d663797b03630b6fc8e719bc4d078873
Behzad hamzei <hamzei.behzad>
2024-04-18 08:41:22 -0400

    add - * / function

--- calc.py
+++ calc.py
@@ -2,7 +2,28 @@ print ("please enter + - * /")
 a=input()
 if a=='+':
     print ("please enter first number")
-    a=input()
+    a=int(input())
     print ("please enter secend number") 
-    b=input()
+    b=int(input())
     print ("the resulte is = ", a+b)
+
+if a=='-':
+    print ("please enter first number")
+    a=int(input())
+    print ("please enter secend number") 
+    b=int(input())
+    print ("the resulte is = ", a-b)
+
+if a=='*':
+    print ("please enter first number")
+    a=int(input())
+    print ("please enter secend number") 
+    b=int(input())
+    print ("the resulte is = ", a*b)
+
+if a=='/':
+    print ("please enter first number")
+    a=int(input())
+    print ("please enter secend number") 
+    b=int(input())
+    print ("the resulte is = ", a/b)
